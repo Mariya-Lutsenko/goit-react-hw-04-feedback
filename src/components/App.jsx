91edc0a0867034f5c65e6f98b71d6d8ec506e224
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-02-04 21:09:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import css from './App.module.css';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
